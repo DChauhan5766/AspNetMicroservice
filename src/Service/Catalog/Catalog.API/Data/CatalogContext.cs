a217b5f919fa863a7cd46f5944cac7efaafe6834
Deepak Chauhan <deepak.chauhan@businessnext.com>
2023-03-07 14:27:06 +0530

    initial

---
+++ src/Service/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration) { 
+
+            var client = new MongoClient(configuration.GetValue<string>("ConnectionStrings:ConnectionString"));
+            var databse = client.GetDatabase(configuration.GetValue<string>("ConnectionStrings: DatabaseName"));
+
+            Products = databse.GetCollection<Product>(configuration.GetValue<string>("ConnectionStrings: CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+
+        }
+         public IMongoCollection<Product> Products { get; }
+    }
+}
+ 
