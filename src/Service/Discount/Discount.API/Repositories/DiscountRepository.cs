e807cf3ecb692a2d8987b295d8d80cd8e6a56782
Deepak Chauhan <deepak.chauhan@businessnext.com>
2023-03-16 16:00:39 +0530

    initial

---
+++ src/Service/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,32 @@
+﻿using Discount.API.Entities;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+        public DiscountRepository(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Coupon> GetDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
