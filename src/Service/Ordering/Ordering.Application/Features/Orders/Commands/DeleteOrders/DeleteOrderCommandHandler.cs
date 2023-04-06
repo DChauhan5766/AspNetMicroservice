a66a4408fc5b04c8658f346eaa039eda594757e8
Deepak Chauhan <deepak.chauhan@businessnext.com>
2023-04-06 15:39:40 +0530

    initital

---
+++ src/Service/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrders/DeleteOrderCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrders
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommands>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommands> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommands> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task Handle(DeleteOrderCommands request , CancellationToken cancellationToken)
+        {
+            var orderdetails = await _orderRepository.GetByIdAsync(request.Id);
+            if (orderdetails == null)
+            {
+                _logger.LogError("Order deleted successfully.");
+            }
+            await _orderRepository.DeleteAsync(orderdetails);
+
+            _logger.LogInformation($"Order {orderdetails.Id} is successfully Deleted.");
+
+            //return Unit.Value;
+        }
+    }
+}
---
+++ src/Service/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckOutOrders;
+using Ordering.Application.Features.Orders.Commands.UpdateOrders;
+using Ordering.Application.Features.Orders.Queries.GetOrderList;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckOutOrderCommands>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
